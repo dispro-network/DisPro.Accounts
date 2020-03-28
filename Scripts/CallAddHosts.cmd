bfc16f4e6fa09a40a57660f66fa83f07ab6dd5f5
Daniel Carrera <daniel.carrera@bycarrera.com>
2020-03-28 16:04:34 -0400

    Set proper dev hosts

--- Scripts/CallAddHosts.cmd
+++ Scripts/CallAddHosts.cmd
@@ -1,6 +1,6 @@
 @echo off
 ECHO Running CallAddHosts
 
-set hosts="accounts.dispro.network.local identity.accounts.dispro.network.local api.accounts.dispro.network.local mvc-implicit.accounts.dispro.network.local mvc-hybrid.accounts.dispro.network.local js.accounts.dispro.network.local react.accounts.dispro.network.local"
+set hosts="accounts.dispro.network.local api.dispro.network.local dispro.network.local"
 CD .\Scripts
 .\AddHosts.cmd %hosts%
